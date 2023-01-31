bbdb091b2801606032086b0921c4e4148e0dc8c6
tokoshika <103026403+tokoshika@users.noreply.github.com>
2023-01-31 14:01:50 +0900

    Add files via upload

---
+++ dist/assets/AboutView-10999cbb.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as s,a as o}from"./index-6e6d7a3a.js";const _={},c={class:"about"},a=o("h1",null,"This is an about page",-1),n=[a];function i(r,u){return t(),s("div",c,n)}const d=e(_,[["render",i],["__file","C:/Users/th/Desktop/VUEROUTER/vuevite/microCMS/src/views/AboutView.vue"]]);export{d as default};
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/microCMStest/' : './',
+  base: process.env.NODE_ENV = 'production' ? '/microCMStest/' : './',
 
   plugins: [vue()],
   resolve: {
