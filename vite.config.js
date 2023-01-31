0e7c544601dddff284757ac509826d3ad056d61e
tokoshika <103026403+tokoshika@users.noreply.github.com>
2023-01-31 13:16:46 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:process.env.NODE_ENV === 'production' ? 'https://tokoshika.github.io/microCMStest/' : './',
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('https://tokoshika.github.io/microCMStest/', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
