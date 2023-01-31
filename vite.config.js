a545ae1bbfc91bdd7edcd98e22edd0acdae80a6f
tokoshika <103026403+tokoshika@users.noreply.github.com>
2023-01-31 13:27:58 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:process.env.NODE_ENV === 'production' ? 'https://tokoshika.github.io/microCMStest/' : './',
+  base:process.env.NODE_ENV === 'production' ? '/microCMStest/' : './',
   plugins: [vue()],
   resolve: {
     alias: {
